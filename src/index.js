0fb14ca2a37db343e701c8f8f6c90d37c4f4db37
Xander Griff <xandgriff@gmail.com>
2017-11-08 19:15:52 -0500

    add index.css for pagewide styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker';
+
+import 'normalize.css';
+import './assets/css/index.css';
 
 import App from './containers/App/App';
 
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
