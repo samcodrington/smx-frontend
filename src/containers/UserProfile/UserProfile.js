4e88cd3de16c22c75036b78625a033484efdc5e3
Sam Codrington <sam.codrington@queensu.ca>
2018-01-20 12:45:24 -0500

    add user info and edit profile button to user page

--- src/containers/UserProfile/UserProfile.js
+++ src/containers/UserProfile/UserProfile.js
@@ -3,22 +3,33 @@
 
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Button from 'material-ui/Button';
+
 
 import UserInfo from '../../components/Userinfo.js';
 class UserProfile extends Component {
   constructor(props) {
     super(props);
     console.log(props);
+    this.state = {
+      user: this.props.user
+    }
   }
 
   componentDidMount() {
-  
+    
+  }
+
+  handleEditProfile(){
+    
   }
 
   render() {
     return (
       <div className='UserInfo'>
-        <UserInfo />
+        <h1> User Profile </h1>
+        <UserInfo user = {this.state.user}/>
+        <Button type = 'submit' onClick = {this.handleEditProfile}> Edit Info </Button>
       </div>
     );
   }
