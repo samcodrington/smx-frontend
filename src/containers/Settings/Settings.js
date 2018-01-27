b70568268021ad13dc12737a4970eeffea28820f
13mtfb <burton.matthew@hotmail.com>
2018-01-27 01:06:10 -0500

    add submitting and routing to backend

--- src/containers/Settings/Settings.js
+++ src/containers/Settings/Settings.js
@@ -12,6 +12,8 @@ import { FormControl, FormHelperText } from 'material-ui/Form';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 
+import UserApi from '../../api/UserApi'
+
 const styles = {
   root: {
     width: '100%',
@@ -44,14 +46,123 @@ class Settings extends Component {
 
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmitDelete = this.handleSubmitDelete.bind(this);
   }
 
+  //reset any potential changes if handleSubmit hasn't been clicked
   handleClick = (event,name) => {
-    this.setState({formType: name});
+    this.setState({
+      formType: name,
+      username: this.props.user.username,
+      nameFirst: this.props.user.nameFirst,
+      nameLast: this.props.user.nameLast,
+      email: this.props.user.email,
+      school: this.props.user.school
+    });
     };
 
   handleChange(event){
-    alert(event.target.value);
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    var dontSend=false
+    event.preventDefault();
+
+    if (this.state.formType=="profile"){
+      //do some basic error checking on entered textbook
+      if (this.state.nameFirst==""){
+        this.setState({nameFirstError: true})
+        dontSend=true
+      } else {this.setState({nameFirstError: false})}
+      if (this.state.nameLast==""){
+        this.setState({nameLastError: true})
+        dontSend=true
+      } else {this.setState({nameLastError: false})}
+      if (this.state.email==""){
+        this.setState({emailError: true})
+        dontSend=true
+      } else {this.setState({emailError: false})}
+      if (this.state.school==""){
+        this.setState({schoolError: true})
+        dontSend=true
+      } else {this.setState({schoolError: false})}
+      if (!dontSend){
+        this.sendUser(1);
+        }
+    }
+    else if (this.state.formType=="account"){
+      //ensure that passwordOld field matches current password and password matches
+      //passwordNew. Also ensure that password was entered
+      if (this.state.username==""){
+        this.setState({userError: true})
+        dontSend=true
+      } else {this.setState({userError: false})}
+
+      if (this.state.password!="" && this.state.passwordOld!="" && this.state.passwordNew!=""){
+        if (this.state.passwordOld!=this.props.user.password){//password entered and incorrect
+          this.setState({passwordOldError: true})
+          dontSend=true
+        } else {this.setState({passwordOldError: false})}
+        if (this.state.password!=this.state.passwordNew){//password don't match
+          this.setState({passwordError: true})
+          dontSend=true
+        } else {this.setState({passwordError: false})}
+      }
+      else {this.setState({password: this.props.user.password})}//pass unchanged password to backend
+
+      if (!dontSend){
+        this.sendUser(2);
+        }
+    }
+  }
+
+  sendUser(useCase){
+    var password
+    if (useCase==1){
+      password=this.props.user.password;
+    }
+    else if (useCase==2){
+      password=this.state.password;
+    }
+    const user = {
+      username: this.state.username,
+      password: password,
+      nameFirst: this.state.nameFirst,
+      nameLast: this.state.nameLast,
+      email: this.state.email,
+      school: this.state.school,
+      _id: this.props.user._id
+    }
+    const response = UserApi
+    .settings(user,useCase)
+    .then((response) => {alert("Settings updated successfully")})//need to update frontend user with new data
+    .catch((response) => {
+        alert('Something went wrong: ' + response.status);
+    });
+  }
+
+  handleSubmitDelete(){
+    const user = {
+      username: this.props.user.username,
+      password: this.props.user.password,
+      nameFirst: this.props.user.nameFirst,
+      nameLast: this.props.user.nameLast,
+      email: this.props.user.email,
+      school: this.props.user.school,
+      _id: this.props.user._id
+    }
+    if (window.confirm('Are you sure you want to delete your account?')) {
+      const response = UserApi
+      .settings(user,3)
+      .then((response) => {alert("Settings updated successfully")})//need to update frontend user with new data
+      .catch((response) => {
+          alert('Something went wrong: ' + response.status);
+      });
+    } else {
+    // Do nothing!
+    }
   }
 
 
@@ -123,10 +234,10 @@ class Settings extends Component {
           </FormControl>
         </Grid>
         <Grid item xs={12}>
-          <Button raised color='primary' style={ styles.button } onClick={ this.handleSubmit }>Submit</Button>
+          <Button raised color='primary' style={ styles.button } onClick={ this.handleSubmit }>Update</Button>
         </Grid>
         <Grid item xs={12}>
-          <Button raised color='secondary' style={ styles.button } onClick={ this.handleSubmit }>Delete Account</Button>
+          <Button raised color='secondary' style={ styles.button } onClick={ this.handleSubmitDelete }>Delete Account</Button>
         </Grid>
       </div>;
     }
