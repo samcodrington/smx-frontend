0e0ac391d69548cf57861045d1389b794dc540b3
Sam Codrington <sam.codrington@queensu.ca>
2018-02-01 19:13:48 -0500

    dynamically display textbookIDs to UserProfile

--- src/containers/UserProfile/UserProfile.js
+++ src/containers/UserProfile/UserProfile.js
@@ -10,6 +10,10 @@ import Button from 'material-ui/Button';
 
 // Components
 import UserInfo from '../../components/Userinfo.js';
+import Textbook from '../../components/Textbook.js';
+
+//API
+import TextbookAPI from '../../api/TextbookApi.js';
 
 class UserProfile extends Component {
   constructor(props) {
@@ -36,11 +40,26 @@ class UserProfile extends Component {
       email: this.state.user.email,
       school: this.state.user.school
     }
+    //Cycle through data to generate textbooks
+    var postedTextbooks = this.props.user.postedtextbooks;
+    postedTextbooks = postedTextbooks.map(id => {
+      //TODO: get textbook info from textbookID
+      return <p> {id} </p>
+      //  <Textbook />
+      
+    });
+    
     return (
-      <div className='UserInfo'>
-        <h1> User Profile </h1>
-        <UserInfo user = {userInfo}/>
-        <Button type = 'submit'><Link to="/settings" style={{ textDecoration: 'none', color: 'black' }}>Settings</Link></Button>
+      <div>
+        <div className='UserInfo'>
+          <h1> User Profile </h1>
+          <UserInfo user = {userInfo}/>
+          <Button type = 'submit'><Link to="/settings" style={{ textDecoration: 'none', color: 'black' }}>Settings</Link></Button>
+        </div>
+        <div className='PostedTextbooks'>
+          <h1> Posted Textbooks </h1>
+            {postedTextbooks}
+        </div>
       </div>
     );
   }
