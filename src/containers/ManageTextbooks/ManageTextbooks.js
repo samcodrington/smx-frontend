321bccc69368584634e27022cda0b75868ab7c14
XanderGriff <xandgriff@gmail.com>
2018-02-11 18:15:37 -0500

    add no textbook msg

--- src/containers/ManageTextbooks/ManageTextbooks.js
+++ src/containers/ManageTextbooks/ManageTextbooks.js
@@ -107,6 +107,14 @@ class ManageTextbooks extends Component {
       );
     }
 
+    if(lengthBooks == 0) {
+    	bookList.push(
+        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+          It doesn't look like you have any books... Post one!
+        </Grid>
+      );
+    }
+
     return(
       <div className='Manage'>
         <Grid container>
